027a39826f391deeda513e05eaa909b9904a4b53
svchnik <svchnik@ukr.net>
2016-07-09 23:56:25 +0300

    new002

--- app/src/main/java/com/example/nik/myapprestaurants/HomeGeocoder.java
+++ app/src/main/java/com/example/nik/myapprestaurants/HomeGeocoder.java
@@ -12,4 +12,45 @@ import android.widget.Toast;
 
 public class HomeGeocoder extends AppCompatActivity {
 
+    final String LOG = "LOG";
+    String strLocation;
+    double mLatitude;
+    double mLongitude;
+    Intent mIntent;
+    Geocoder geocoder;
+    List<Address> addresses;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addresses = null;
+        strLocation = getIntent().getStringExtra("mStrLocation");
+        geocoder = new Geocoder(getApplicationContext());
+
+        try {
+            addresses = geocoder.getFromLocationName(strLocation, 10);
+            //for (int i = 0; i < addresses.size(); i++) {
+            if (!addresses.isEmpty()) {
+                Address loc = addresses.get(0);
+                mLatitude = loc.getLatitude();
+                mLongitude = loc.getLongitude();
+
+                mIntent = new Intent();
+                mIntent.putExtra("Lat", mLatitude);
+                mIntent.putExtra("Lon", mLongitude);
+                setResult(RESULT_OK, mIntent);
+                finish();
+            } else {
+                Toast.makeText(this, "No location results", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "GeoCoder error", Toast.LENGTH_SHORT).show();
+            Log.d(LOG, "----------------------onCreate GeoCoder error");
+        }
+
+    }
 }
--- app/src/main/java/com/example/nik/myapprestaurants/MyGoogleGPSCoordinate.java
+++ app/src/main/java/com/example/nik/myapprestaurants/MyGoogleGPSCoordinate.java
@@ -12,6 +12,91 @@ import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
-public class MyGoogleGPSCoordinate extends AppCompatActivity {
+public class MyGoogleGPSCoordinate extends AppCompatActivity
+        implements  GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
 
+
+    private GoogleApiClient mGoogleApiClient;
+    LocationRequest mLocationRequest;
+    double mLatitude;
+    double mLongitude;
+    Location mLocation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        buildGoogleApiClient();
+        onStart();
+    }
+
+    //*****************implements 4 metods
+    @Override
+    public void onConnected(Bundle bundle) {
+
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(100);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+
+        Intent mIntent = new Intent();
+        mLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+        if(mLocation != null){
+            mLatitude = mLocation.getLatitude();
+            mLongitude = mLocation.getLongitude();
+
+            mIntent.putExtra("Lat",mLatitude);
+            mIntent.putExtra("Lon",mLongitude);
+            setResult(RESULT_OK, mIntent);
+            finish();
+
+        }
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        buildGoogleApiClient();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+    }
+    //**********************************
+
+
+    synchronized void buildGoogleApiClient(){
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mGoogleApiClient.disconnect();
+        super.onDestroy();
+    }
 }
